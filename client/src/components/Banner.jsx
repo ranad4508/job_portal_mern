f6f0494e74032b6aaecce114de9d56eed5c11fd4
ranad4508 <ranad4508@gmail.com>
2024-06-17 21:07:47 +0545

    jobsathi completed

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = ({ handleInputChange, query }) => {
       </p>
 
       <form className="">
-        <div className="flex justify-start md:flex-row flex-col md:gap-0 gap-4">
+        <div className="flex justify-center md:flex-row flex-col md:gap-0 gap-4">
           <div className="flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2 w-full">
             <input
               type="text"
@@ -24,19 +24,6 @@ const Banner = ({ handleInputChange, query }) => {
               value={query}
             />
             <FiSearch className="absolute mt-2.5 ml-2 text-gray-400" />
-          </div>
-
-          <div className="flex md:rounded-none rounded ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3">
-            <input
-              type="text"
-              name="username"
-              id="username"
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder="Location"
-              onChange={handleInputChange}
-              value={""}
-            />
-            <FiMapPin className="absolute mt-2.5 ml-2 text-gray-400" />
           </div>
 
           <button
